f04ad1f700cd15d111575b3621c19b849e4eea24
Bullock <qwerty983@mail.ru>
2019-02-25 10:04:33 +0300

    Update DateTimeExtentionMethods.cs

--- DateTimeExtentionMethods.cs
+++ DateTimeExtentionMethods.cs
@@ -6,12 +6,6 @@ namespace UsefulExtentions {
 				throw new ArgumentException("One or both arguments are not initialized.");
 			}
 
-			if (oneDate > twoDate) {
-				var swap = oneDate;
-				oneDate = twoDate;
-				twoDate = swap;
-			}
-
 			int monthsApart = 12 * (oneDate.Year - twoDate.Year) + oneDate.Month - twoDate.Month;
 			return Math.Abs(monthsApart);
 		}
